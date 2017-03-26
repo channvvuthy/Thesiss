604c1f90888cc66e55d0204e50a37f1c6c1daba3
chann <vuthychan271@yahoo.com>
2017-03-26 19:56:46 +0700

    update LoginController date 3/26/2017

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    /*
+     * Login function
+     */
+    public function postLogin(Request $request){
+        
+    }
 }
